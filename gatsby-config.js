ccc72368cb8c2c36a0ab7112573b29acd7a4ca2d
Sebastian Zelonka <dev@sebazelonka.com>
2023-01-04 13:39:08 -0300

    updated dependencies and removed netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,71 +1,64 @@
 module.exports = {
-    siteMetadata: {
-        title: "Sebastian Zelonka v2018",
+  siteMetadata: {
+    title: "Sebastian Zelonka v2018",
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
+    {
+      // keep as first gatsby-source-filesystem plugin for gatsby image support
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/static/img`,
+        name: "uploads",
+      },
     },
-    plugins: [
-        "gatsby-plugin-react-helmet",
-        "gatsby-plugin-sass",
-        {
-            // keep as first gatsby-source-filesystem plugin for gatsby image support
-            resolve: "gatsby-source-filesystem",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/img`,
+        name: "images",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-relative-images",
             options: {
-                path: `${__dirname}/static/img`,
-                name: "uploads",
+              name: "uploads",
             },
-        },
-        {
-            resolve: "gatsby-source-filesystem",
+          },
+          {
+            resolve: "gatsby-remark-images",
             options: {
-                path: `${__dirname}/src/pages`,
-                name: "pages",
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 2048,
             },
+          },
+        ],
+      },
+    },
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        google: {
+          families: ["Exo 2:700,900", "Merriweather:300,400,700"],
         },
-        {
-            resolve: "gatsby-source-filesystem",
-            options: {
-                path: `${__dirname}/src/img`,
-                name: "images",
-            },
-        },
-        "gatsby-plugin-sharp",
-        "gatsby-transformer-sharp",
-        {
-            resolve: "gatsby-transformer-remark",
-            options: {
-                plugins: [
-                    {
-                        resolve: "gatsby-remark-relative-images",
-                        options: {
-                            name: "uploads",
-                        },
-                    },
-                    {
-                        resolve: "gatsby-remark-images",
-                        options: {
-                            // It's important to specify the maxWidth (in pixels) of
-                            // the content container as this plugin uses this as the
-                            // base for generating different widths of each image.
-                            maxWidth: 2048,
-                        },
-                    },
-                ],
-            },
-        },
-        {
-            resolve: "gatsby-plugin-netlify-cms",
-            options: {
-                modulePath: `${__dirname}/src/cms/cms.js`,
-            },
-        },
-        "gatsby-plugin-styled-components",
-        {
-            resolve: "gatsby-plugin-web-font-loader",
-            options: {
-                google: {
-                    families: ["Exo 2:700,900", "Merriweather:300,400,700"],
-                },
-            },
-        },
-        "gatsby-plugin-netlify", // make sure to keep it last in the array
-    ],
+      },
+    },
+  ],
 };
